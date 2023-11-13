c6aed52a4e405ad2c9803c192bec302c3877796d
Juan Pastelin <pastelin406@gmail.com>
2023-11-12 20:26:27 -0600

    Implements Stream Java 17

---
+++ java-masterclass/JavaProgrammingMasterclass/src/section_17_streams/students/Course.java
@@ -0,0 +1,19 @@
+package section_17_streams.students;
+
+public record Course(String courseCode, String title, int lectureCount) {
+
+    public Course {
+        if (lectureCount <= 0) {
+            lectureCount = 1;
+        }
+    }
+
+    public Course(String courseCode, String title) {
+        this(courseCode, title, 40);
+    }
+
+    @Override
+    public String toString() {
+        return "%s %s".formatted(courseCode, title);
+    }
+}
